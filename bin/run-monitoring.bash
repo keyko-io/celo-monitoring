69590d33fb1df8801f9b19aada8cde2a202697bb
jose pablo <jose@oceanprotocol.com>
2020-03-02 18:46:42 +0100

    creating elastic template before starting kafka connect

--- bin/run-monitoring.bash
+++ bin/run-monitoring.bash
@@ -113,15 +113,15 @@ if [[ $COMMAND == *"config"* ]]; then
     echo -e "\n\n* Configuring Monitoring Agent\n"
     newman run $__PARENT_DIR/docs/postman/agent-api.postman_collection.json
 
+    echo -e "\n\n* Configuring Elastic Dynamic Template\n"
+    curl -X PUT -u $ELASTIC_USER:$ELASTIC_PASSWORD -H "Content-Type: application/json"  --data @$__DIR/http-request-elastic-template.txt http://$ELASTIC_URL/_template/monitoring_dynamic_template
+
     echo -e "\n\n* Configuring Kafka Connect Driver\n"
 
     curl -X POST -H "Content-Type: application/json" --data @$__CONF_DIR/connect/connect-config.txt http://$CONNET_URL/connectors
     curl -X POST -H "Content-Type: application/json" --data @$__CONF_DIR/connect/contract-blocks-connector.txt http://$CONNET_URL/connectors
     curl -X POST -H "Content-Type: application/json" --data @$__CONF_DIR/connect/contract-events-connector.txt http://$CONNET_URL/connectors
     curl -X POST -H "Content-Type: application/json" --data @$__CONF_DIR/connect/contract-views-connector.txt http://$CONNET_URL/connectors
-
-    echo -e "\n\n* Configuring Elastic Dynamic Template\n"
-    curl -X PUT -u $ELASTIC_USER:$ELASTIC_PASSWORD -H "Content-Type: application/json"  --data @$__DIR/http-request-elastic-template.txt http://$ELASTIC_URL/_template/monitoring_dynamic_template
 
     echo -e "\n\n* Configuring Kibana Dashboard\n"
     curl -X POST -u $ELASTIC_USER:$ELASTIC_PASSWORD "$KIBANA_URL/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@$__CONF_DIR/kibana/kibana-dashboard.ndjson
